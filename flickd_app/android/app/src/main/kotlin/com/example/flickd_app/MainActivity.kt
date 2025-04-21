48948d3d5adc506694ade158ea2dc4eb476465d6
Sahaj Raj Malla <51092972+sahajrajmalla@users.noreply.github.com>
2025-04-21 12:34:55 +0545

    :tada: Initial Basic App created

---
+++ flickd_app/android/app/src/main/kotlin/com/example/flickd_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flickd_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ flickd_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("Sahaj Raj Malla"),
+      centerTitle: truhaj Raj Malla"),
+      centerTitle: true,
+      backgroundColor: Colors.green,
+    ),
+    body: Center(
+      child: Text("Hello World!"),
+    ),
+  )
+));e,
+      backgroundColor: Colors.green,
+    ),
+    body: Center(
+      child: Text("Hello World!"),
+    ),
+  )
+));
