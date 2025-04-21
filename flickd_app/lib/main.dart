e028b228a961cc81727809de5cca88957836f038
Sahaj Raj Malla <51092972+sahajrajmalla@users.noreply.github.com>
2025-04-21 18:43:41 +0545

    :tada: working code

--- flickd_app/lib/main.dart
+++ flickd_app/lib/main.dart
@@ -4,15 +4,7 @@ void main() => runApp(MaterialApp(
   home: Scaffold(
     appBar: AppBar(
       title: Text("Sahaj Raj Malla"),
-      centerTitle: truhaj Raj Malla"),
       centerTitle: true,
-      backgroundColor: Colors.green,
-    ),
-    body: Center(
-      child: Text("Hello World!"),
-    ),
-  )
-));e,
       backgroundColor: Colors.green,
     ),
     body: Center(
