93a05dcafbf2c17e63c0b0282ddcbb089737a3be
Sahaj Raj Malla <51092972+sahajrajmalla@users.noreply.github.com>
2025-04-22 15:59:40 +0545

    :recycle: added preety dart and fix UI

--- flickd_app/lib/dummy_prac.dart
+++ flickd_app/lib/dummy_prac.dart
@@ -5,27 +5,23 @@ void main() {
 
   print(myList);
 
-
   int age = 11;
 
   bool isCorrect = true;
 
-  if (isCorrect)  {
+  if (isCorrect) {
     print("It is correct!");
-
   }
-
 
   String name = "Sahaj Raj Malla";
 
   print(name);
 
-
-  for (int i =0; i<10; i++)  {
+  for (int i = 0; i < 10; i++) {
     print("Hello");
   }
 
-  if (age > 18)  {
+  if (age > 18) {
     print("You can vote.");
   } else {
     print("You can't vote.");
@@ -41,41 +37,24 @@ void main() {
 
   print(kta.age);
   print(kta.isMale);
-
 }
 
-
-String greetings()  {
+String greetings() {
   return "Hello Fucker";
 }
 
-class Person  {
-
-
-
+class Person {
   int age = 0;
 
-  Person(int a)  {
-    this.age = a;
+  Person(int a) {
+    age = a;
   }
-
 }
 
 class Male extends Person {
   bool isMale = true;
 
-  Male(int a, bool s) : super(a)  {
-
-    this.isMale = s;
-
+  Male(super.a, bool s) {
+    isMale = s;
   }
-
-
 }
-
-
-
-
-
-
-
--- flickd_app/lib/main.dart
+++ flickd_app/lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: NinjaCard(),
-));
-
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: NinjaCard()));
 
 class NinjaCard extends StatelessWidget {
+  const NinjaCard({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,66 +30,59 @@ class NinjaCard extends StatelessWidget {
                 radius: 40.0,
               ),
             ),
-            Divider(
-              height: 90.0,
-                color: Colors.grey[800],
-            ),
+            Divider(height: 90.0, color: Colors.grey[800]),
             Text(
-            'NAME',
-            style: TextStyle(
-              color: Colors.grey,
-              letterSpacing: 2.0,
+              'NAME',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
             ),
-          ),
             SizedBox(height: 10.0),
             Text(
-              'Sahaj Raj Malla',
+              'Shree Sahaj Raj Malla',
               style: TextStyle(
-                color: Colors.amberAccent[200],
+                color: const Color.fromARGB(255, 64, 255, 124),
                 letterSpacing: 2.0,
                 fontSize: 28.0,
-                fontWeight: FontWeight.bold
+                fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 30.0,),
+            SizedBox(height: 30.0),
             Text(
               'CURRENT NINJA LEVEL',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
+              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
             ),
             SizedBox(height: 10.0),
             Text(
               '10',
               style: TextStyle(
-                  color: Colors.amberAccent[200],
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 30.0,),
+            SizedBox(height: 30.0),
+            Text(
+              'NINJA EMAIL',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+            ),
+            SizedBox(height: 10.0),
             Row(
               children: <Widget>[
-                Icon(
-                  Icons.email_outlined,
-                  color: Colors.grey[400],
+                Icon(Icons.email_outlined, color: Colors.grey[400]),
+                SizedBox(height: 10.0, width: 10.0),
+                Text(
+                  'mallasahajraj@gmail.com',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
                 ),
-            SizedBox(height: 10.0, width: 10.0,),
-            Text(
-                'mallasahajraj@gmail.com',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.0,
-                  letterSpacing: 1.0
-                ),
-            ),
-
               ],
-            )
+            ),
           ],
         ),
-      ));
+      ),
+    );
   }
 }
